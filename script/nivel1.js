7ae5324f34ad82d5cd653c4aef7023eadfd1c94f
ffukasawa <fefs.fuka@gmail.com>
2025-09-02 17:50:15 -0300

    ajeitando o score

---
+++ script/nivel1.js
@@ -0,0 +1,30 @@
+
+duracao = 1 * 60 + 30;
+
+function main (){
+    
+    window.addEventListener('load', function() {
+    comecarJogo();
+    mainInterval = window.setInterval(function(){
+        deslizarAnimais();
+        gerarAnimais();
+        if (controle === 3){
+           controle =  0;
+        }
+        matchAnimais(5);
+        matchAnimais(4);
+        matchAnimais(3); 
+
+        if(gameOver){
+            controle = 3;
+            handleGameOver(100);
+        } 
+    },100);
+    });
+        timerInterval = setInterval(() => {
+                atualizarTimer(duracao)
+            
+        }, 1000);
+    }
+
+main();
